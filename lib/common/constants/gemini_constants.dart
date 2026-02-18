124f53fe2a25eb193dc512e1042e8982f47899ea
fahim <fahimfaizabir25@gmail.com>
2026-02-18 22:45:35 +0600

    [fix] change timeout to 180s

--- lib/common/constants/gemini_constants.dart
+++ lib/common/constants/gemini_constants.dart
@@ -4,5 +4,5 @@ class GeminiConstants {
   static const String modelName = 'gemini-3-flash-preview';
   static const double temperature = 0.3;
   static const int maxOutputTokens = 4096;
-  static const Duration timeout = Duration(seconds: 30);
+  static const Duration timeout = Duration(seconds: 180);
 }
